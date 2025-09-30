0457c1262dfa1685b58baec0396fb532c72b1e89
princepkkumar <pkssjr43@gmail.com>
2025-09-30 22:17:29 +0530

    Final: Force update Dockerfile and entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-# This script is the official entrypoint for the Docker container.
-# It runs the Uvicorn server using its absolute path to be 100% sure.
-
 /root/.local/bin/uvicorn src.main:app --host 0.0.0.0 --port 10000
